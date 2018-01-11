7d854104d33be38019804207d3a23d272151b84d
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2018-01-10 22:14:44 -0800

    updated new article to allow image centering and resize changes

--- src/components/pages/auth/NewArticle.vue
+++ src/components/pages/auth/NewArticle.vue
@@ -91,6 +91,38 @@ import { quillEditor, Quill } from 'vue-quill-editor'
 import ImageResize from 'quill-image-resize-module'
 Quill.register('modules/imageResize', ImageResize)
 
+const BaseImageFormat = Quill.import('formats/image')
+const ImageFormatAttributesList = [
+  'alt',
+  'height',
+  'width',
+  'style'
+]
+
+class ImageFormat extends BaseImageFormat {
+  static formats (domNode) {
+    return ImageFormatAttributesList.reduce(function (formats, attribute) {
+      if (domNode.hasAttribute(attribute)) {
+        formats[attribute] = domNode.getAttribute(attribute)
+      }
+      return formats
+    }, {})
+  }
+  format (name, value) {
+    if (ImageFormatAttributesList.indexOf(name) > -1) {
+      if (value) {
+        this.domNode.setAttribute(name, value)
+      } else {
+        this.domNode.removeAttribute(name)
+      }
+    } else {
+      super.format(name, value)
+    }
+  }
+}
+
+Quill.register(ImageFormat, true)
+
 export default {
   name: 'NewArticle',
   components: {
